1722d9d44bec706b7ba77bf6b79c0b673144a08d
Roberto <robertomelo287@gmail.com>
2023-08-10 01:37:21 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './global.css'
+import S from './App.module.css'
+import { Header } from './components/Header/Header'
+import { Post } from './components/Posters/Post'
+import { Sidebar } from './components/Sidebar/Sidebar'
+
+function App() {
+
+  return (
+    <>
+    <div>
+      <Header />
+      <div className={S.warppaer}>
+        <Sidebar />        
+        <main>
+          <Post />
+        </main>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
