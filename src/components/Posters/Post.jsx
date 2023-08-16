40f024e0f6b6dec221d7c2fb65e30c093fdbba3c
Roberto <robertomelo287@gmail.com>
2023-08-16 01:35:43 -0300

    feat: adjust of elements in component post

--- src/components/Posters/Post.jsx
+++ src/components/Posters/Post.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
 // import { Avatar } from '../Avatar/Avatar'
 import { Avatar } from '../Avatar/Avatar'
 import { Comment } from '../Comment/Comment'
@@ -6,35 +7,32 @@ import S from './Post.module.css'
 export function Post() {
   return (
     <article className={S.post}>
-      <div className={S.header}>
-        <div className={S.profile}>
+      <header>
+        <div className={S.author}>  
           <Avatar hasBorder src='https://avatars.githubusercontent.com/u/67019242?v=4' /> 
-            <div>
-              <p className={S.name}>Christian Valentim</p>
-              <p className={S.profission}>Dev Frontend</p>
-            </div>
+          <div className={S.authorInfo}>
+            <strong>Roberto Melo</strong>
+            <span>Web Developer</span>
+          </div>
         </div>
-          <div className={S.status}><p>Publicado em 1h</p></div>
-      </div>
-      <main className={S.content}>
+        <time dateTime='14/18/2023 00:13'>Publicado a cerca de 1h</time>
+      </header>
+      <div className={S.content}>
         <p> Fala galeraa </p>
         <p> 👋 Acabei de subir mais um projeto no meu portifa. </p>
-        <p>  É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀 </p>
-        <div className={S.author}>
-            <a className={S.anchor} href="https://github.com/RobertodeOliveira">👉 Jane.desing/doctorcare</a>
-          <p className={S.portfolio}>  
-            <a  className={S.anchor}href="">#novoprojeto</a>{' '} 
-            <a  className={S.anchor}href="">#nlw</a> {' '}
-            <a className={S.anchor} href="">#rocketseat</a> {' '}
-          </p>
-        </div>
-        <p></p>
-      </main>
-      <footer >
-        <p className={S.mensagem}>Deixe seu Feedback</p>
-        <input className={S.input} type="text" placeholder='Nossa, adorei amigo!' />
-      </footer>
-      <Comment />
+        <p>  É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀 </p> 
+        <p><a className={S.anchor} href="https://github.com/RobertodeOliveira">👉 Jane.desing/doctorcare</a></p> 
+        <a  className={S.anchor}href="">#novoprojeto</a>{' '} 
+        <a  className={S.anchor}href="">#nlw</a> {' '}
+        <a className={S.anchor} href="">#rocketseat</a> {' '}
+      </div>
+      <form className={S.comentForm}>
+        <strong>Deixe seu feedback</strong>
+        <textarea placeholder='Deixe seu comentário' />
+        <footer>
+          <button type='submit'>Comentar</button>
+        </footer>
+      </form>
       <Comment />
     </article>
   )
